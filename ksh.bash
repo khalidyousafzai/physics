777d9bf7b71fb32e79fb1869c554a793880dc633
khalidyousafzai <khalidyousafzai@hotmail.com>
2023-06-04 12:31:47 +0500

    update

---
+++ ksh.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+git clone https://www.github.com/khalidyousafzai/machines-latex
+git clone https://github.com/khalidyousafzai/digital-circuits-libre-office
+git clone https://github.com/khalidyousafzai/electronics-latex
+git clone https://github.com/khalidyousafzai/electromagnetics-latex
+git clone https://github.com/khalidyousafzai/electric-circuit-analysis-latex
+git clone https://github.com/khalidyousafzai/engineering-Mathematics
+git clone https://github.com/khalidyousafzai/calculus-and-analytic-geometry
+git clone https://github.com/khalidyousafzai/quantum-mechanics.git
+git clone https://github.com/khalidyousafzai/matricMathsFirst.git
+git clone https://github.com/khalidyousafzai/indexFormatter.git
+git clone https://github.com/khalidyousafzai/machines-pushto-latex.git
